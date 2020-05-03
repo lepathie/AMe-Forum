6462d53e1df26bcdd1e2a64e10b21b258d466bda
Leon Pascal Thierschmidt <leon-pascal.thierschmidt@outlook.de>
2020-05-04 01:24:08 +0200

    lint

--- server/imports/methods/topics.ts
+++ server/imports/methods/topics.ts
@@ -2,8 +2,8 @@ import { Topics } from 'imports/collections/topics'
 import _ from 'underscore'
 
 if (Topics.find().cursor.count() === 0) {
-        _.each(['General Discussion', 'Tutorials', 'Help'],
-               (topicName) => {
-                                Topics.insert({ name: topicName})
-        })
-    }
+    _.each(['General Discussion', 'Tutorials', 'Help'],
+           (topicName) => {
+        Topics.insert({ name: topicName })
+    })
+}
--- server/imports/publications/topics.ts
+++ server/imports/publications/topics.ts
@@ -3,10 +3,10 @@ import { Meteor } from 'meteor/meteor'
 import { Topics } from '../../../imports/collections/topics'
 
 Meteor.publish('topics', () => {
-        return Topics.find()
-    })
+    return Topics.find()
+})
 
 Meteor.publish('topic', (id) => {
-        check(id, String)
-        return Topics.find({ _id: id})
-    })
+    check(id, String)
+    return Topics.find({ _id: id})
+})
