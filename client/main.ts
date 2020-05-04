bf5528d75def875bd0c018479c2ee2affee78923
Leon Pascal Thierschmidt <leon-pascal.thierschmidt@outlook.de>
2020-05-04 15:00:35 +0200

    lint

--- client/main.ts
+++ client/main.ts
@@ -1,16 +1,16 @@
-import './imports/polyfills';
-import { Meteor } from 'meteor/meteor';
+import './imports/polyfills'
+import { Meteor } from 'meteor/meteor'
 
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './imports/app/app.module';
+import { enableProdMode } from '@angular/core'
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+import { AppModule } from './imports/app/app.module'
 
 Meteor.startup(() => {
 
   if (Meteor.isProduction) {
-    enableProdMode();
+    enableProdMode()
   }
 
-  platformBrowserDynamic().bootstrapModule(AppModule);
+  platformBrowserDynamic().bootstrapModule(AppModule)
 
-});
+})
