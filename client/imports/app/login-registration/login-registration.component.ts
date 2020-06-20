b9c0e826594a31f478140e10d30fd67e64b2b854
Leon Pascal Thierschmidt <leon-pascal.thierschmidt@outlook.de>
2020-06-20 18:16:00 +0200

    Removed Changedetectionstrategy

--- client/imports/app/login-registration/login-registration.component.ts
+++ client/imports/app/login-registration/login-registration.component.ts
@@ -1,10 +1,10 @@
-import { Component, ChangeDetectionStrategy, Input } from "@angular/core"
+import { Component } from "@angular/core"
 
+// tslint:disable-next-line: prefer-on-push-component-change-detection
 @Component({
   selector: "login-registration-component",
   templateUrl: "./login-registration.html",
-  styleUrls: ["./login-registration.scss"],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ["./login-registration.scss"]
 })
 export class LoginRegistrationComponent {
 }
