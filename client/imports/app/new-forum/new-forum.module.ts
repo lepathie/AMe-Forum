133c2bc5dd526e9e83e398d697b0c03d50f7b027
Julian Retzbach <allaround.jr@gmail.com>
2020-06-21 00:19:23 +0200

    unimportant imports deleted
    -

--- client/imports/app/new-forum/new-forum.module.ts
+++ client/imports/app/new-forum/new-forum.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from "@angular/core"
 import { NewForumComponent } from './new-forum.component'
-import { FooterModule } from '../footer/footer.module'
-import { HeaderModule } from '../header/header.module'
 import { SharedModule } from '../shared/shared.module'
 import { ReactiveFormsModule } from '@angular/forms'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatInputModule } from '@angular/material/input'
-import { MatSelectModule } from '@angular/material/select'
 import { MatButtonModule } from '@angular/material/button'
-import { MatCheckboxModule } from '@angular/material/checkbox'
-import { MatChipsModule } from '@angular/material/chips'
-import { BrowserModule } from '@angular/platform-browser'
 import { FrameModule } from '../frame/frame.module'
-import { Routes, RouterModule } from '@angular/router'
+import { RouterModule } from '@angular/router'
 
 @NgModule({
     declarations: [
@@ -20,19 +14,12 @@ import { Routes, RouterModule } from '@angular/router'
     ],
     imports: [
         SharedModule,
-        HeaderModule,
-        FooterModule,
-        BrowserModule,
         ReactiveFormsModule,
         BrowserAnimationsModule,
         MatInputModule,
-        MatSelectModule,
         MatButtonModule,
-        MatCheckboxModule,
-        MatChipsModule,
         FrameModule,
         RouterModule
-
     ],
     exports: [
         NewForumComponent
