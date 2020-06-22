7439191de142a65b4710b7d6325115d81b51a552
Leon Pascal Thierschmidt <leon-pascal.thierschmidt@outlook.de>
2020-06-22 08:22:13 +0200

    Minor changes

--- server/imports/methods/defaults.ts
+++ server/imports/methods/defaults.ts
@@ -1,20 +1,23 @@
 import { Forums } from 'imports/collections/forums'
 import _ from 'underscore'
+import { Threads } from 'imports/collections/threads'
+import { Posts } from 'imports/collections/posts'
 /**
  *  @file This file contains the defaults for this AMe-Forum installation
  *  Feel free to customize for your needs.
  */
+
+ // Default Forums, Threads and Posts
 if (Forums.find().cursor.count() === 0) {
   _.each([{ active: true, name: "Help", description: "Contains tutorials on usage of the forums." }, { active: true, name: "General", description: "Introduction for new users and general rules."} ], (forum) => {
     Forums.insert(forum)
   })
-  // Threads.insert({ _id: "TestId", forumId: "TestId" , name: "testThread", viewCounter: 0, followCounter: 0, date: new Date(), creator: "auto"})
-  // Posts.insert({ _id: "TestId", threadId: "TestId", quotedPost: "", textQuoted: "", content: "TestKommentar", userId: "auto", postTime: new Date()})
 
-  // Methoden um zum erstellen eines Forums:
-  // const idforum = Meteor.call("createForum", "Forum1", "Testforum", "TestModeratorId")
-  // const idthread = Meteor.call("createThread", "cwRruXbjRuD47PTf3", "Testthread")
-  // const idpost = Meteor.call("createPost", "T9aLNqvAjfL87MoGW", "", "", "Kommentar........")
+  Forums.insert({ active: true, name: "Feature-Showcase", description: "Contains some showcases of features."}).subscribe((_forumId) => {
+      Threads.insert({ forumId: _forumId , name: "Markdown Support", viewCounter: 0, followCounter: 0, date: new Date(), creator: "System"}).subscribe((_threadId) => {
+        Posts.insert({ threadId: _threadId, userId: "System", postTime: new Date(), content: "# We support Markdown 🙂\n\nYou can use **Markdown** in all of your posts.  \nMake 'em *pretty*!"})
+      })
+  })
 }
 // Default users
 if (Meteor.users.find().count() === 0) {
@@ -58,5 +61,4 @@ if (Meteor.users.find().count() === 0) {
         Roles.addUsersToRoles(id, user.roles)
       }
     )
-  }
-})
+}
